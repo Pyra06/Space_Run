d91b7b61d96745101386b2e3548c4df21a438f98
Pyra06 <aniruddhasharma867@gmail.com>
2022-07-17 10:58:19 +0530

    Space Run

---
+++ Space-Run/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject blockPrefab;
+    private float spawnTime = 2f;
+    public float intervalTime = 1f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time >= spawnTime)
+        {
+            Spawner();
+            spawnTime = Time.time + intervalTime;
+        }
+    }
+
+    void Spawner()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Space-Run/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 15f;
+    public float mapWidth = 4f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        Vector3 newPostion = rb.position + Vector3.right * x;
+
+        newPostion.x = Mathf.Clamp(newPostion.x, -mapWidth, mapWidth);
+
+        rb.MovePosition(newPostion);
+    }
+}
